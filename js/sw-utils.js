8cbc7e801fbe92e6ae1bf720a10d01d4a6112b36
Jorge Sendarrubias <jorgesendarrubias@hotmail.com>
2022-01-06 14:40:43 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,18 @@
+
+
+const actualizaCacheDinamico = (dynamicCache, request, response) => {
+
+    if (response.ok) {
+
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(request, response.clone());
+
+            return response.clone();
+        });
+
+    }
+    else {
+        return response;
+    }
+
+}
